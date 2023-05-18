e9d8495fa0106f23fad5239aec8cff8ff4fa317e
Neudo Paulino <neudinho66@gmail.com>
2023-05-18 20:08:21 -0300

    fix properties: Propriedades de prod

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -19,6 +19,13 @@ public class ApiApplication {
 		 * delete from flyway_schema_history where success = 0;
 		 * drop database vollmed_api;
 		 * create database vollmed_api;
+		 *
+		 * COMANDOS JAVA:
+		 * dir target/
+		 * java -jar .\target\api-0.0.1-SNAPSHOT.jar
+		 * java -Dspring.profiles.active=prod -jar .\target\api-0.0.1-SNAPSHOT.jar
+		 * java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://localhost/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=mysql -jar .\target\api-0.0.1-SNAPSHOT.jar
+		 *
 		 */
 	}
 
